e25b58dae61e0d9d2eead025b94d89aedda31882
Elisabeth Aparecida <beth_ap@live.com>
2024-01-01 16:55:33 -0300

    Configuração do Projeto

---
+++ src/main/java/com/generation/novaquitanda/NovaquitandaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.novaquitanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaquitandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaquitandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/novaquitanda/NovaquitandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.novaquitanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaquitandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
